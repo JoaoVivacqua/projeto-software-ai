0dbbf2afd398a33b2e0d9556da501a4aecbbb4eb
João Vivacqua <joaopbvp@al.insper.edu.br>
2026-09-22 22:52:15 -0300

    Projeto base

---
+++ src/main/java/com/projetosoftware/ai/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.projetosoftware.ai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+}
